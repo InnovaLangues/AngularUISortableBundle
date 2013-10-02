2cfc2b844c0f068021006b4f8cf1584e71895775
Elorfin <penin.axel@gmail.com>
2013-10-02 12:06:12 +0200

    First commit : Bundle structure

---
+++ InnovaAngularUISortableBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Innova\AngularUISortableBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+
+class InnovaAngularUISortableBundle extends Bundle implements AutoConfigurableInterface
+{
+    public function supports($environment)
+    {
+        return true;
+    }
+    
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
+}
