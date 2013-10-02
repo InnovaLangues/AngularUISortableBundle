b7c4bc1e8441f234da10099e5e945b927f98b182
Axel Penin <penin.axel@gmail.com>
2013-10-02 14:27:52 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Innova\AngularPageslideBundle\DependencyInjection;
+namespace Innova\AngularUISortableBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('innova_angular_pageslide');
+        $rootNode = $treeBuilder->root('innova_angular_uisortable');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
