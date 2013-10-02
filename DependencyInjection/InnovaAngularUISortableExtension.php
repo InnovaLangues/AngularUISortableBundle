30dc4e1ec237cf493a0cab42fb470c0cecd904e2
Axel Penin <penin.axel@gmail.com>
2013-10-02 14:48:51 +0200

    Update InnovaAngularUISortableExtension.php

--- DependencyInjection/InnovaAngularUISortableExtension.php
+++ DependencyInjection/InnovaAngularUISortableExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class InnovaUISortableExtension extends Extension
+class InnovaAngularUISortableExtension extends Extension
 {
     /**
      * {@inheritDoc}
