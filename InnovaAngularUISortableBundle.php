2260e6b91701d6e975a14f0868955aa1df22db2d
Nicolas Dufour <nico@nico>
2015-05-26 11:00:32 +0200

    Upgrade claro 5

--- InnovaAngularUISortableBundle.php
+++ InnovaAngularUISortableBundle.php
@@ -3,18 +3,8 @@
 namespace Innova\AngularUISortableBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
-use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class InnovaAngularUISortableBundle extends Bundle implements AutoConfigurableInterface
+class InnovaAngularUISortableBundle extends Bundle
 {
-    public function supports($environment)
-    {
-        return true;
-    }
     
-    public function getConfiguration($environment)
-    {
-        return new ConfigurationBuilder();
-    }
 }
